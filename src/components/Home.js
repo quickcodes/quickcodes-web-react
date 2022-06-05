706fb7b77996f0cbfd2f62fefd383d1352ab7479
quickcodes <dhruvcodes7220@gmail.com>
2022-06-05 22:46:49 +0530

    Basic Page Made

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Home() {
   const FIRST_NAME = 'Dhruv';
-  const LAST_NAME = 'SONI';
+  const LAST_NAME = 'Soni';
   const INTRO_HEAD = 'Hi, I am QuickCodes';
   const INTRO_DESC =
     'I am Dhruv Soni, aka quickcodes. I am a 19 old guy from India, who loves Computers and Software. I am also a Computer Science Student.';
@@ -17,38 +17,39 @@ export default function Home() {
 
   return (
     <div>
-      <h1>
-        <span class="big_pink">{FIRST_NAME}</span>
-        <span class="big_purple">&nbsp;{LAST_NAME}</span>
-      </h1>
-      <div className="centre">
-        <input type="text" placeholder="Search Blogs" />
-        <button>Go</button>
-      </div>
-      <div className="centre">
-        <p className="quotes">
-          <b>"</b>
-          {QUOTES}
-          <b>"</b>
-        </p>
-      </div>
+      <header>
+        <h1>
+          <span className="big_pink">{FIRST_NAME}</span>
+          <span className="big_purple">&nbsp;{LAST_NAME}</span>
+        </h1>
+        <div className="centre">
+          <input type="text" placeholder="Search Blogs" />
+          <button>Go</button>
+        </div>
+        <div className="centre">
+          <p className="quotes">
+            <b>"</b>
+            {QUOTES}
+            <b>"</b>
+          </p>
+        </div>
 
-      <div className="centre">
-        {nav_bar.map((x) => (
-          <span class="nav-button">
-            [ <a href="/">{x.name}</a> ]
-          </span>
-        ))}
-      </div>
+        <div className="centre">
+          {nav_bar.map((x) => (
+            <span className="nav-button">
+              [ <a href="/">{x.name}</a> ]
+            </span>
+          ))}
+        </div>
+      </header>
 
-      <p className="centre">
-        __________________________________________________________
-      </p>
-      <h2>{INTRO_HEAD}</h2>
+      <main>
+        <h2>{INTRO_HEAD}</h2>
 
-      <p class="intro">{INTRO_DESC}</p>
+        <p className="intro">{INTRO_DESC}</p>
 
-      {/* <p>hello</p> */}
+        <p>this is text</p>
+      </main>
     </div>
   );
 }
