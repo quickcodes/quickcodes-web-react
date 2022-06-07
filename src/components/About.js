bbb5278baf5bdc1ddcb77ce15f6fec1512a4cd92
quickcodes <dhruvcodes7220@gmail.com>
2022-06-07 12:31:25 +0530

    1 file modified

--- src/components/About.js
+++ src/components/About.js
@@ -5,19 +5,13 @@ export default function About() {
   return (
     <div className="half-screen">
       <Header />
-      <h2 className="start white coding-font">
+      <h2 className="start green coding-font tables-points">
         What more do you wanna know about me?
       </h2>
 
-      <ul className="coding-font">
+      <ul className="tables-points coding-font">
         <li>
-          Maybe you are expecting somthing like - My name is
-          <span className="blue"> $FULL_Name </span>I am
-          <span className="blue"> $PROFESSION </span>
-          and i live in
-          <span className="blue"> $CITY. </span>
-          But Putting different variables in the same line each time can't
-          introduce anyone.
+        When someone asks Tell me about yourself? It almost feels like someone allowed them to sing an old song. Same lyrics with some variables in it.
         </li>
 
         <li>
