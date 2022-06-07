a100f331e8cd2d1473363ac88d6d0ad54fa5d38b
quickcodes <dhruvcodes7220@gmail.com>
2022-06-07 12:57:46 +0530

    1 file modified

--- src/components/About.js
+++ src/components/About.js
@@ -3,27 +3,36 @@ import Header from './utils/header';
 
 export default function About() {
   return (
-    <div className="half-screen">
+    <div >
       <Header />
-      <h2 className="start green coding-font tables-points">
-        What more do you wanna know about me?
-      </h2>
 
-      <ul className="tables-points coding-font">
-        <li>
-        When someone asks Tell me about yourself? It almost feels like someone allowed them to sing an old song. Same lyrics with some variables in it.
-        </li>
+      {/* <div className="flex"> */}
+        <div className="half-left">
+          <h2 className="start green coding-font tables-points">
+            What more do you wanna know about me?
+          </h2>
 
-        <li>
-          Maybe you are expecting somthing like - My name is
-          <span className="blue"> $FULL_Name </span>I am
-          <span className="blue"> $PROFESSION </span>
-          and i live in
-          <span className="blue"> $CITY. </span>
-          But Putting different variables in the same line each time can't
-          introduce anyone.
-        </li>
-      </ul>
+          <ul className="tables-points coding-font">
+            <li>
+            When someone asks Tell me about yourself? It almost feels like someone allowed them to sing an old song. Same lyrics with some variables in it.
+            </li>
+
+            <li>
+              Maybe you are expecting somthing like - My name is
+              <span className="blue"> $FULL_Name </span>I am
+              <span className="blue"> $PROFESSION </span>
+              and i live in
+              <span className="blue"> $CITY. </span>
+              But Putting variables in a shity old 90's line will not define who you are right?
+            </li>
+          </ul>
+        </div>
+
+        {/* <div className="half-right">
+          some text
+        </div> */}
+      {/* </div> */}
+
     </div>
   );
 }
